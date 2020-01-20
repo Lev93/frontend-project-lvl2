c431ddf2b7e2c0b973cc8145732896973adeadfb
Lev Riabov <levriabov12345@mail.ru>
2020-01-20 12:43:02 +0300

    шаг 2

--- src/program.js
+++ src/program.js
@@ -4,6 +4,11 @@ const program = () => {
   commander
     .description('Compares two configuration files and shows a difference.')
     .option('-v, --version', 'output usage information')
+    .option('-f, --format <type>', 'output format [pretty]', 'pretty')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(`${firstConfig}${secondConfig}`);
+    })
     .parse(process.argv);
 };
 
